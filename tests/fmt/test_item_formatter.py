6003b2b8cfcd4a8f86783521cdd0d80050026f31
KKlochko <KKlochko@protonmail.com>
2023-06-20 19:28:19 +0300

    Remove the method to get name and count for 'addItem' command.

--- tests/fmt/test_item_formatter.py
+++ tests/fmt/test_item_formatter.py
@@ -36,26 +36,3 @@ def test_format_items():
 
     assert expected_format == items_fmt
 
-def test_get_name_and_count_from_parts():
-    name_and_count = 'Дуже довга назва страви, 10'
-
-    expected_name = 'Дуже довга назва страви'
-    expected_count = 10
-
-    formatter = ItemFormatter()
-
-    item = formatter.get_name_and_count_from_parts(name_and_count)
-    item_name, item_count = item['name'], item['count']
-
-    assert expected_name == item_name
-    assert expected_count == item_count
-
-def test_get_name_and_count_from_parts_invalid_command():
-    name_and_count = 'Дуже довга назва страви 10'
-
-    formatter = ItemFormatter()
-
-    error = formatter.get_name_and_count_from_parts(name_and_count)
-
-    assert 'Не правильний формат' == error['error']
-
