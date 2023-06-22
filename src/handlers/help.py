9bf2d8d41ba246e277b71880dc315a3a7c93e3cc
KKlochko <KKlochko@protonmail.com>
2023-06-22 11:24:29 +0300

    Update the help.

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -2,9 +2,25 @@ import simplematrixbotlib as botlib
 
 
 async def help_handler(room_id: str, bot: botlib.Bot, sender: str, admin_id: str):
-    msg: str = 'Hi **{}**\n\n' \
-               'Prefix: `!`\n' \
-               'Its a simplematrixbotlib template!\n' \
-               'Admin contact: **{}**'
+    msg: str = 'Привіт, **{}**\n\n' \
+               'Префікс команда: `!`\n' \
+               '[dots-matrix-bot](https://gitlab.com/KKlochko/dots-matrix-bot) - це вільне програмне забезпечення за лацензією GNU AGPLv3 чи будь-яка новіша версія.\n' \
+               'Контакт адміна: **{}**\n\n' \
+               '\n\nКоманди:\n' \
+               'help - допомога.\n' \
+               'register - пов\'язати користувача та акаунт матриці.\n' \
+               'Приклад: `!register username 3806712312121`.\n\n' \
+               'city-list - перелік міст.\n' \
+               'company-list - перелік компаній.\n' \
+               'category-list - перелік категорій та товарів.\n' \
+               'cart - перелік товарів у кошику та їх вартість.\n\n' \
+               'select-city - обрати місто.\n' \
+               'Приклад: `!select-city Чернігів`.\n\n' \
+               'select-company - обрати компанію.\n' \
+               'Приклад: `!select-company Назва Компанії`.\n\n' \
+               'add-item - додати товар у кошик.\n' \
+               'Приклад: `!add-item !add-item Назва товару, 2`.\n\n' \
+               'order - замовити.'
 
     await bot.api.send_markdown_message(room_id=room_id, message=msg.format(sender, admin_id))
+
