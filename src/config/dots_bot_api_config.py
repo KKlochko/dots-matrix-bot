3bb09413b1a664ce50573d3640e6e95f6b22adc6
KKlochko <KKlochko@protonmail.com>
2023-07-18 15:53:35 +0300

    Update the config class for the dots bot api.

--- src/config/dots_bot_api_config.py
+++ src/config/dots_bot_api_config.py
@@ -1,5 +1,10 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
 class DotsBotApiConfig():
-    _base_url = "https://domain"
+    _base_url = env('BOT_API_BASE_URL')
 
     def __str__(self) -> str:
         return f"{self._base_url=}"
